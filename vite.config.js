96003bd974f1b15f714147062ac509d4f7b333b3
xuanxiang <xuanxiang001@126.com>
2026-05-13 18:10:27 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // 拆分第三方依赖（如 react/vue/axios 等）
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    },
+    // 可选：压缩产物，减少体积
+    minify: 'esbuild'
+  }
 })
